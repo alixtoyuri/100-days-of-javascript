057272b552a238695fb1e1cb958ba11aaeb1f910
alixtoyuri <alixtoyuri@gmail.com>
2026-08-04 18:43:06 -0600

    Create variablesTiposDatos.js

---
+++ variablesTiposDatos.js
@@ -0,0 +1,34 @@
+//DECLARAR UNA VARIABLE
+let numero = 10;
+
+//MOSTRAR UN NOMBRE
+let name= "Juana";
+console.log(name);
+
+//SUMAR DOS NUMEROS
+let numOne= 5;
+let numTwo= 10;
+let suma= numOne + numTwo;
+
+console.log(suma);
+
+//RESTAR
+let numOne= 5;
+let numTwo= 10;
+let resta= numOne - numTwo;
+
+console.log(resta);
+
+//MULTIPLICAR
+let numOne= 5;
+let numTwo= 10;
+let multiplicar= numOne * numTwo;
+
+console.log(multiplicar);
+
+//DIVIDIR
+let numOne= 5;
+let numTwo= 10;
+let div= numOne / numTwo;
+
+console.log(div);
