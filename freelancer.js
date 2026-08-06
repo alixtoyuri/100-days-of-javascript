14f774ceb5e409ce076824217db54acd9bbd1e0e
Alix <alixtoyuri@gmail.com>
2026-08-06 15:50:05 -0600

    Create freelancer.js

---
+++ freelancer.js
@@ -0,0 +1,54 @@
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return ratePerHour * 8;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour));
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+
+
+
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  //tarifa diaria
+  const dailyRate = dayRate(ratePerHour);
+
+  // Meses completos
+  const fullMonths = Math.floor(numDays / 22);
+
+  //días restantes
+  const remainingDays = numDays % 22;
+
+  //Costo de los meses con descuento
+  const monthlyCost = 22 * dailyRate * (1 - discount);
+
+  /*
+  
+  */
+  const total =
+    fullMonths * monthlyCost +
+    remainingDays * dailyRate;
+
+  return Math.round(total);
+}
