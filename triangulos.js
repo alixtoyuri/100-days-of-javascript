815c96ec3c45cf18d6b7cb476047a60f82ea2208
Alix <alixtoyuri@gmail.com>
2026-08-10 15:13:15 -0600

    Create triangulos.js

---
+++ triangulos.js
@@ -0,0 +1,27 @@
+numOne = 5;
+numTwo = 5;
+numThree = 5;
+
+if (numOne === numTwo === numThree){
+    console.log("Equilatero")
+} if else (numOne===numTwo!==numThree||numOne!==numTwo===numThree||numThree===numOne!==numTwo){
+    console.log("Isósceles")
+} if else (numOne !== numTwo !== numThree){
+    console.log("Escaleno")
+}
+
+
+
+numOne = 55;
+numTwo = 15;
+numThree = 5;
+
+if (numOne === numTwo && numTwo===numThree && numOne===numThree){
+    console.log("Equilatero");
+} 
+else if (numOne===numTwo || numTwo===numThree || numOne!==numTwo || numOne!==numTwo || numTwo!==numThree){
+    console.log("Isósceles");
+} 
+else {
+    console.log("Escaleno");
+}
