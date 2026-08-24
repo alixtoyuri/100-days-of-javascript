8c2c464b216def93d83114484414bedfe8694ee0
Alix <alixtoyuri@gmail.com>
2026-08-24 17:51:23 -0600

    Create fizz-buzz.js

---
+++ fizz-buzz.js
@@ -0,0 +1,21 @@
+/*
+3. FizzBuzz pequeño
+
+Crea:
+
+fizzBuzz(num)
+
+Reglas:
+
+divisible entre 3 → "Fizz"
+divisible entre 5 → "Buzz"
+divisible entre ambos → "FizzBuzz"
+ninguno → el número como string
+
+Ejemplos:
+
+3  → "Fizz"
+5  → "Buzz"
+15 → "FizzBuzz"
+7  → "7"
+*/
