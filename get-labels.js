e2d50ff44fe4dcfb7bd3a70970806af817f6e57e
Alix <alixtoyuri@gmail.com>
2026-08-25 14:02:10 -0600

    Create get-labels.js

---
+++ get-labels.js
@@ -0,0 +1,28 @@
+/*
+6. Etiquetas para un número
+
+Crea:
+
+getLabels(num)
+
+Añade:
+
+divisible entre 2 → "Even"
+divisible entre 3 → "Three"
+divisible entre 5 → "Five"
+divisible entre 10 → "Ten"
+
+Por ejemplo:
+
+6  → "EvenThree"
+
+10 → "EvenFiveTen"
+
+30 → "EvenThreeFiveTen"
+
+7  → "7"
+
+Aquí empiezas a enfrentarte a algo importante:
+
+una condición puede implicar que varias etiquetas sean correctas.
+*/
