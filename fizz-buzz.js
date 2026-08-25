27c8066a8e94e3ffa885fe3cd89314703825c9d8
Alix <alixtoyuri@gmail.com>
2026-08-25 13:24:54 -0600

    Update fizz-buzz.js

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -19,6 +19,8 @@ Ejemplos:
 15 → "FizzBuzz"
 7  → "7"
 */
+
+//Primera Solución
 let num = Number(prompt ("Digite un número: "));
 let result="";
 
@@ -31,4 +33,20 @@ if(num%5=== 0){
 if(num === ""){
   return String(num);
 }
+
+
+//Segunda Solución
+let num = Number(prompt ("Digite un número: "));  
+let result="";
+
+if(num % 3 === 0){
+  result += "Fizz";
+}
+if(num%5=== 0){
+  result += "Buzz";
+}
+if(result === ""){
+  console.log( num);
+}
+ console.log(result);
 
