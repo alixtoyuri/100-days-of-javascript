3d4906361b4d6c4b3be9a2cfdc8b1bf8527a373e
Alix <alixtoyuri@gmail.com>
2026-08-28 12:18:46 -0600

    Update and-basico.js

--- and-basico.js
+++ and-basico.js
@@ -2,3 +2,6 @@
 Ejercicio 2 — AND básico
 Escribe puedeConducir(tieneLicencia, esMayorDeEdad) que devuelva true solo si tiene licencia y es mayor de edad.
 */
+export function puedeConducir(tieneLicencia, esMayorDeEdad) {
+return tieneLicencia && esMayorDeEdad;
+}
