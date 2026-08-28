13432a949c3f5ba3f8bb372d23ed883256233ec9
Alix <alixtoyuri@gmail.com>
2026-08-28 12:30:38 -0600

    Update and-negacion.js

--- and-negacion.js
+++ and-negacion.js
@@ -2,3 +2,6 @@
 Ejercicio 4 — AND con negación
 Escribe puedeNadar(saboNadar, elAguaEstaFria) que devuelva true si sabe nadar y el agua no está fría.
 */
+export function puedeNadar(saboNadar, elAguaEstaFria) {
+return saboNadar && !elAguaEstaFria;
+}
