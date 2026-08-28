c373ef8169980dc24285c13fe6c7110c6bcaf784
Alix <alixtoyuri@gmail.com>
2026-08-28 12:14:12 -0600

    Update negacion-simple.js

--- negacion-simple.js
+++ negacion-simple.js
@@ -2,3 +2,9 @@
 Ejercicio 1 — Negación simple
 Escribe estaCerrado(estaAbierto) que devuelva true si la tienda está cerrada (recibe si está abierta).
 */
+
+let desicion = Number (prompt ("Digite 1 si es verdad y 0 si no \n¿La tienda está cerrada? "));
+
+export function cestaCerrado(estaAbierto) {
+return !estaAbierta;
+}
