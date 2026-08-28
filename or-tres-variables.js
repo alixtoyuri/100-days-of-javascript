e163f5510226effaa5115de028af452911723108
Alix <alixtoyuri@gmail.com>
2026-08-28 12:31:59 -0600

    Update or-tres-variables.js

--- or-tres-variables.js
+++ or-tres-variables.js
@@ -2,3 +2,6 @@
 Ejercicio 5 — OR con tres variables
 Escribe necesitaParaguas(estaLloviendo, vaANevar, hayViento) que devuelva true si ocurre al menos una de las tres condiciones.
 */
+export function necesitaParaguas(estaLloviendo, vaANevar, hayViento) {
+return estaLloviendo || vaANevar || hayViento;
+}
